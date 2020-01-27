2622c36c643299ac7e2aa5a1f0c9f8d8360d04db
markus-zhang <markus_zhang@hotmail.com>
2020-01-27 17:59:23 -0500

    A bunch of changes

---
+++ src/input.py
@@ -0,0 +1,22 @@
+import tcod as libtcod
+
+def handle_key_pressed(key: libtcod.Key)-> dict:
+    # Movements
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, +1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (+1, 0)}
+    
+    # Toggle Fullscreen
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        return {'fullscreen': True}
+
+    # Quit application
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
+    return {}
